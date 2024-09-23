ced93b6b207f498ee19bc2962a377f024df304cf
Bkr4wczynski <bkrawczynski@protonmail.com>
2024-09-23 19:57:04 +0200

    fully functioning program but untested and unpatched

--- src/main/java/com/Bartek/springActivity/SpringActivityApplication.java
+++ src/main/java/com/Bartek/springActivity/SpringActivityApplication.java
@@ -4,21 +4,39 @@ import com.Bartek.springActivity.rest.ApiCaller;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.*;
 
 @SpringBootApplication
 public class SpringActivityApplication {
 	private final ApiCaller apiCaller = new ApiCaller();
+	private final Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringActivityApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner() {
+	public CommandLineRunner commandLineRunner(ConfigurableApplicationContext context) {
 		return runner -> {
-			System.out.println(apiCaller.callGetRequest("users/bkr4wczynski"));
+			System.out.println("Please provide github username");
+			String username = scanner.nextLine();
+			List apiResponse = apiCaller.callGetRequest("users/"+username+"/events");
+			System.out.println("Output: ");
+			for (Object activity : apiResponse) {
+				System.out.println(formatApiResponse((LinkedHashMap) activity));
+			}
+			context.close();
+			return;
 		};
+	}
+	private String formatApiResponse(LinkedHashMap activity) {
+		String type = activity.get("type").toString();
+		String repoName = ((LinkedHashMap<?, ?>) activity.get("repo")).get("name").toString();
+		String createdAt = activity.get("created_at").toString();
+		return "- " + type + " in repository: " + repoName + " at: " + createdAt;
 	}
 
 }
--- src/main/java/com/Bartek/springActivity/rest/ApiCaller.java
+++ src/main/java/com/Bartek/springActivity/rest/ApiCaller.java
@@ -2,13 +2,16 @@ package com.Bartek.springActivity.rest;
 
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
+import java.util.Map;
+
 public class ApiCaller {
     RestClient restClient = RestClient.create();
 
-    public String callGetRequest(String uri) {
+    public List callGetRequest(String uri) {
         return restClient.get()
                 .uri("https://api.github.com/"+uri)
                 .retrieve()
-                .body(String.class);
+                .body(List.class);
     }
 }
